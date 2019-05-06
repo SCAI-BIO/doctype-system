9ad63d17f7df5207bb44face3b25243578795a27
daria.podorskaja@gmail.com <daria.podorskaja@gmail.com>
2019-05-06 13:59:20 +0200

    rows and headers added

--- src/main/java/de/fraunhofer/scai/bio/types/text/doc/structure/Table.java
+++ src/main/java/de/fraunhofer/scai/bio/types/text/doc/structure/Table.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2018 Fraunhofer Institute SCAI, St. Augustin, Germany
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,6 +16,8 @@
 package de.fraunhofer.scai.bio.types.text.doc.structure;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A captioned table.
@@ -30,60 +32,116 @@ public class Table implements Serializable {
     private TextElement title;
     private TextElement text;
     private TextElement rhetorical;
+    private List<TextElement> headers;
+    private List<List<TextElement>> columns;
+    private List<Map<String, String>> rows;
+    private String tableId;
+
+
+
 
     /**
      * @return the caption
      */
-    public TextElement getCaption() {
-	return caption;
+    public TextElement getCaption()
+    {
+        return caption;
     }
 
     /**
      * @return the text
      */
-    public TextElement getText() {
-	return text;
+    public TextElement getText()
+    {
+        return text;
     }
 
     /**
      * @return the title
      */
-    public TextElement getTitle() {
-	return title;
+    public TextElement getTitle()
+    {
+        return title;
     }
 
     /**
      * @return the rhetorical
      */
-    public TextElement getRhetorical() {
+    public TextElement getRhetorical()
+    {
         return rhetorical;
     }
 
     /**
      * @param rhetorical the rhetorical to set
      */
-    public void setRhetorical(TextElement rhetorical) {
+    public void setRhetorical(TextElement rhetorical)
+    {
         this.rhetorical = rhetorical;
     }
 
-    public void setCaption(TextElement caption) {
-	this.caption = caption;
+    public void setCaption(TextElement caption)
+    {
+        this.caption = caption;
     }
 
-    public void setText(TextElement text) {
-	this.text = text;
+    public void setText(TextElement text)
+    {
+        this.text = text;
     }
 
-    public void setTitle(TextElement title) {
-	this.title = title;
+    public void setTitle(TextElement title)
+    {
+        this.title = title;
     }
 
     /* (non-Javadoc)
      * @see java.lang.Object#toString()
      */
     public String toString() {
-    	return String.format("%s\n%s\n%s", title.getText(), text.getText(), caption.getText());
+    	return String.format("%s", tableId);
     }
 
 
+    public List<TextElement> getHeaders()
+    {
+        return headers;
+    }
+
+    public void setHeaders(List<TextElement> headers)
+    {
+        this.headers = headers;
+    }
+
+
+    public List<List<TextElement>> getColumns()
+    {
+        return columns;
+    }
+
+    public void setColumns(List<List<TextElement>> columns)
+    {
+        this.columns = columns;
+    }
+
+
+    public List<Map<String, String>> getRows()
+    {
+        return rows;
+    }
+
+    public void setRows(List<Map<String, String>> rows)
+    {
+        this.rows = rows;
+    }
+
+    public String getTableId()
+    {
+        return tableId;
+    }
+
+    public void setTableId(String tableId)
+    {
+        this.tableId = tableId;
+    }
 }
