331404ceffe7a3180b7a0e8befea1f06b021203b
Johannes Darms <johannes.darms@scai.fraunhofer.de>
2020-08-17 09:15:57 +0200

    changed Author back to List and mark List as don't merge

--- src/main/java/de/fraunhofer/scai/bio/types/text/doc/meta/Bibliographic.java
+++ src/main/java/de/fraunhofer/scai/bio/types/text/doc/meta/Bibliographic.java
@@ -21,6 +21,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonMerge;
+import com.fasterxml.jackson.annotation.OptBoolean;
+
 import de.fraunhofer.scai.bio.types.text.doc.structure.TextElement;
 
 import lombok.Data;
@@ -36,7 +39,8 @@ import lombok.Data;
      */
     private static final long serialVersionUID = 7449243668495438139L;
     private Abstract documentAbstract;
-    private Set<Author> authors;
+    @JsonMerge(OptBoolean.FALSE)
+    private List<Author> authors;
     private Date pubDate;
     private Set<License> licenses;
     private Title title;
@@ -50,7 +54,7 @@ import lombok.Data;
      */
     public void addAuthor(Author author) {
         if (this.authors == null) {
-            this.authors = new HashSet<>();
+            this.authors = new ArrayList<>();
         }
         this.authors.add(author);
     }
