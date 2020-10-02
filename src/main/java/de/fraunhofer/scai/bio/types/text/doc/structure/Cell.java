6cac0ea6c05e6f79647d8ab4b69617aa8d4f9c28
tadams <tim-adams@gmx.net>
2020-10-02 09:22:46 +0200

    Add license

--- src/main/java/de/fraunhofer/scai/bio/types/text/doc/structure/Cell.java
+++ src/main/java/de/fraunhofer/scai/bio/types/text/doc/structure/Cell.java
@@ -1,10 +1,28 @@
+/*
+ * Copyright 2018 Fraunhofer Institute SCAI, St. Augustin, Germany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.fraunhofer.scai.bio.types.text.doc.structure;
 
 import java.io.Serializable;
 
+import lombok.Data;
+
 /**
  * @author tadams
  */
+@Data
 public class Cell implements Serializable {
 
     private static final long serialVersionUID = -2815310382355431825L;
@@ -16,44 +34,4 @@ public class Cell implements Serializable {
     TextElement content;
     CellType type;
 
-
-    public int getRow() {
-        return row;
-    }
-
-    public void setRow(int row) {
-        this.row = row;
-    }
-
-    public int getCol() {
-        return col;
-    }
-
-    public void setCol(int col) {
-        this.col = col;
-    }
-
-    public int getRowSpan() {
-        return rowSpan;
-    }
-
-    public void setRowSpan(int rowSpan) {
-        this.rowSpan = rowSpan;
-    }
-
-    public int getColSpan() {
-        return colSpan;
-    }
-
-    public void setColSpan(int colSpan) {
-        this.colSpan = colSpan;
-    }
-
-    public TextElement getContent() {
-        return content;
-    }
-
-    public void setContent(TextElement content) {
-        this.content = content;
-    }
 }
